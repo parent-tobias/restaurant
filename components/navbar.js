62f1af79c713774d7ffff1a85df481b5f57fac19
Haru-hue <53390409+Haru-hue@users.noreply.github.com>
2022-03-17 14:11:27 +0100

    Dribble Page completed

---
+++ components/navbar.js
@@ -0,0 +1,28 @@
+class Header extends HTMLElement {
+    constructor () {
+        super();
+    }
+    connectedCallback () {
+        this.innerHTML = `<header class = "container-fluid">
+        <nav class = "navbar navbar-expand-lg d-flex">
+            <a href="#" class = "navbar-brand ps-4"><img src="./images/ChickenRepublic_Logo.svg" class = "img-fluid"></a>
+            <ul class = "navbar-nav ms-auto pe-4" id="menu">
+                <li class="nav-item">
+                    <a href="index.html" class="nav-link" id="home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="menu.html" class="nav-link" id="order">Menu</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" id="location">Store Location</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" id="about">Brand</a>
+                </li>
+            </ul>
+        </nav>
+        </header>`
+    }
+}
+
+customElements.define('header-component', Header)
--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-var arImages = ["ChickenRepublic_BannerIcon2-1.png", "ChickenRepublic_BannerIcon3-1.png", 
-"ChickenRepublic_BannerIcon4-1.png", "ChickenRepublic_BannerIcon5-1.png", 
-"ChickenRepublic_BannerIcon6-1.png", "ChickenRepublic_BannerIcon7-3.png", 
+var arImages = ["ChickenRepublic_BannerIcon3-1.png", "ChickenRepublic_BannerIcon2-1.png", 
+"ChickenRepublic_BannerIcon4-1.png", "ChickenRepublic_BannerIcon6-1.png", 
+"ChickenRepublic_BannerIcon5-1.png", "ChickenRepublic_BannerIcon7-3.png", 
 ]
-var url = "../images/"
-var menu = document.querySelectorAll(".grid > div::before")
+var url = "./images/"
+var menu = document.querySelectorAll(".grid > div")
+
+menu.forEach((item, index) => {
+    var img = document.createElement("img")
+    img.src = url + arImages[index]
+    item.appendChild(img);
+})
 
 var orderMenu = [ "Soulfully Spiced Fried Chicken", "Delicious Rotisserie Chicken",
-"Flame Grilled Chicken", "Burgers, Wraps & ChickWhizz",
-"Tasty Sides", "Sweet Treats & Beverages"]
-var orders = document.querySelectorAll(".grid > div")
-orders.forEach((item, i) => {
-    item.textContent = orderMenu[i]
+"Flame Grilled Chicken", "Tasty Sides",
+"Burgers, Wraps & ChickWhizz", "Sweet Treats & Beverages"]
+menu.forEach((item, i) => {
+    var text = document.createElement("span")
+    text.innerText = orderMenu[i]
+    item.appendChild(text);
 })
+
+var links = document.querySelectorAll(".navbar-nav .nav-link")
+// links.forEach((navclick,_,nav) => {
+//     navclick.onclick = () => 
+//         nav.forEach(n => {
+//             n.classList.toggle("active",n===navclick)
+//         })
+// })
+
+links.forEach((link)=>{
+    if(window.location.href === link.href) {
+        link.classList.add("active")
+    }
+})
+
