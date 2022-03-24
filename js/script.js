eee7f1a4f5df6ae2093ad8a06b933be274a57c6e
Toby Parent <parent.tobias@gmail.com>
2022-03-24 08:53:14 -0400

    checking data import

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
-fetch('../sources/items.json')
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            appendData(data);
-        })
-        .catch(function (err) {
-            console.log('error: ' + err);
-        });
+import * as data from '../sources/items.json';
 
-    function appendData(data) {
-        let mainContainer = document.getElementById("soulful");
-        for (let i = 0; i < data.length; i++) {
-            let div = document.createElement("div");
-            div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].lastName;
-            mainContainer.appendChild(div);
-        }
+function appendData(data) {
+    let mainContainer = document.getElementById("soulful");
+    for (let i = 0; i < data.length; i++) {
+        let div = document.createElement("div");
+        div.innerHTML = 'Name: ' + data[i].name + ' ' + data[i].lastName;
+        mainContainer.appendChild(div);
     }
+}
+console.log(data);
+appendData(data);
